28f04b4c4e37931ec237d9d042079926781b7a0d
LesyaLarina <69485004+LesyaLarina@users.noreply.github.com>
2022-09-01 14:05:57 +0300

    Change variables

---
+++ lesson3_task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// 1.  Обмен переменными
+// Создайте программу, что будет запрашивать два числа у пользователя. 
+// Полученные данные поместите в переменные. Поменяйте местами значения в переменных.
+
+using System;
+
+namespace lesson3_task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите два числа:");
+            short num1 = Convert.ToInt16(Console.ReadLine());
+            short num2 = Convert.ToInt16(Console.ReadLine());
+            short temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.WriteLine($"Если поменять местами, будет {num1} и {num2}");
+        }
+    }
+}
