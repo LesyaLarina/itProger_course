f37d787e065e7b2bac2a45b4247c46430f6d30f7
LesyaLarina <69485004+LesyaLarina@users.noreply.github.com>
2022-09-01 18:20:39 +0300

    Remainder

---
+++ lessons3-5_task6/Program.cs
@@ -0,0 +1,20 @@
+﻿// Остаток при деление
+// Попросите пользователя ввести два числа. 
+// Выполните операцию остаток от деления и выведите результат в консоль.
+
+using System;
+
+namespace lessons3_5_task6
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите первое число: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Остаток от деления равен {num1%num2}");
+        }
+    }
+}
