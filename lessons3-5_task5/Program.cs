41eb7c59a35e61d6e347125c46a2f971ee9a96e9
LesyaLarina <69485004+LesyaLarina@users.noreply.github.com>
2022-09-01 18:13:50 +0300

    Work with variables

---
+++ lessons3-5_task5/Program.cs
@@ -0,0 +1,22 @@
+﻿// Работа с переменными
+// Создайте программу, что будет запрашивать данные пользователя: 
+// имя, фамилию, возраст и далее будет выводить их на экран.
+
+using System;
+
+namespace lessons3_5_task5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите фамилию: ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Введите возраст: ");
+            byte age = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine($"Вы {name} {surname}, вам {age}");
+        }
+    }
+}
