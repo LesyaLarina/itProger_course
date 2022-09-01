b226f2d922a9a86c8584c35e471ade5cf230f6c4
LesyaLarina <69485004+LesyaLarina@users.noreply.github.com>
2022-09-01 15:01:51 +0300

    Value types

---
+++ lesson3_task7/Program.cs
@@ -0,0 +1,22 @@
+﻿// 7.  Разные типы переменных
+// Создайте переменные для хранения следующий значений: -34, 4, 'R', 23.093433, 40000, true, 0. 
+// Продумайте типы данных для переменных, чтобы они максимально подходили под каждое значение.
+
+using System;
+
+namespace lesson3_task7
+{
+    class Program
+    {
+        static void Main()
+        {
+            sbyte num1 = -34;
+            byte num2 = 4;
+            char symb = 'R';
+            float num3 = 23.093433f;
+            int num4 = 40000;
+            bool smth = true;
+            byte zero = 0;
+        }
+    }
+}
