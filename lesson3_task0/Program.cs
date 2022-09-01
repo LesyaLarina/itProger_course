0caf0a8746be53438ee4829b9c9277a9522e9483
LesyaLarina <69485004+LesyaLarina@users.noreply.github.com>
2022-09-01 15:48:59 +0300

    Creating variables

---
+++ lesson3_task0/Program.cs
@@ -0,0 +1,22 @@
+﻿// Создайте несколько переменных для хранения таких значений, как: -34, "Hello", 'R', 23.093433222, 
+// 30000, true, 0. Продумайте типы данных для переменных, чтобы они максимально подходили 
+// под каждое значение.
+
+using System;
+
+namespace lesson3_task0
+{
+    class Program
+    {
+        static void Main()
+        {
+            sbyte num1 = -34;
+            string word = "Hello";
+            char symb = 'R';
+            float num2 = 23.093433222f;
+            short num3 = 30000;
+            bool smth = true;
+            byte zero = 0;
+        }
+    }
+}
