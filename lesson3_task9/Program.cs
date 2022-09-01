06e29c083ce3c9916f8d9564cd26eef3b2563e74
LesyaLarina <69485004+LesyaLarina@users.noreply.github.com>
2022-09-01 15:33:45 +0300

    Converting

---
+++ lesson3_task9/Program.cs
@@ -0,0 +1,26 @@
+﻿// 9.  Преобразование данных
+// Получите от пользователя данные и запишите их в переменные с типами данных:
+// short
+// ulong
+// char
+// double. 
+
+using System;
+
+namespace lesson3_task9
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число от -32768 до 32767:");
+            short num1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Введите очень большое положительное число:");
+            ulong num2 = Convert.ToUInt64(Console.ReadLine());
+            Console.WriteLine("Введите символ:");
+            char symb = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Введите большое число со знаками после запятой:");
+            double num3 = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
