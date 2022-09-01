65f6ee96ffc624f7c830c5423054f57aa66fda85
LesyaLarina <69485004+LesyaLarina@users.noreply.github.com>
2022-09-01 14:34:52 +0300

    Work with variables

---
+++ lesson3_task4/Program.cs
@@ -0,0 +1,20 @@
+﻿// 4.  Работа с переменными
+// Попросите пользователя ввести имя, а также возраст.
+// Выведите полученные данные в формате: «Привет, ИМЯ! Тебе уже ВОЗРАСТ».
+
+using System;
+
+namespace lesson3_task4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите свое имя:");
+            string username = Console.ReadLine();
+            Console.WriteLine("Введите свой возраст:");
+            byte userage = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine($"Привет, {username}! Тебе уже {userage}");
+        }
+    }
+}
