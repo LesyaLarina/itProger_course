e023c5b4b5fe0ff77529d799370179e7ebc7cf72
LesyaLarina <69485004+LesyaLarina@users.noreply.github.com>
2022-09-01 17:09:11 +0300

    Domain name

---
+++ lessons3-5_task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Доменное имя
+// Попросите пользователя ввести URL какого-либо сайта. После ввода данных выведите в консоль доменное имя полученного URL адреса.
+// Пример:
+// // Получил значение - itproger.com
+// // в консоль будет выведено - com
+// Подсказка: стоит использовать функцию split.
+
+using System;
+
+namespace lessons3_5_task2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите URL сайта");
+            string urlname = Console.ReadLine();
+            string[] urldot = urlname.Split(new char[]{'.'});
+            Console.WriteLine(urldot[urldot.Length-1]);
+        }
+    }
+}
