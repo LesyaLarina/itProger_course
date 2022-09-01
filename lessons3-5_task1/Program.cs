69011430a0bf986218676254cc91f009561cd3a0
LesyaLarina <69485004+LesyaLarina@users.noreply.github.com>
2022-09-01 16:36:44 +0300

    Field of symbols

---
+++ lessons3-5_task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Поле из символов
+// Напишите программу, которая будет рисовать поле из символа, который введет пользователь. 
+// Пример:
+// // Пользователь вводит 4
+// // В итоге выводиться следующее
+// 4444
+// 4  4
+// 4  4
+// 4444
+
+using System;
+
+namespace lessons3_5_task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите символ:");
+            char symb = Convert.ToChar(Console.Read());
+            Console.WriteLine($"{symb}{symb}{symb}{symb}");
+            Console.WriteLine($"{symb}  {symb}");
+            Console.WriteLine($"{symb}  {symb}");
+            Console.WriteLine($"{symb}{symb}{symb}{symb}");
+        }
+    }
+}
