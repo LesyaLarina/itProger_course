b0fc23db1001fec74e311dfd83168f9745d9abb5
LesyaLarina <69485004+LesyaLarina@users.noreply.github.com>
2022-09-01 14:53:33 +0300

    Find Number

---
+++ lesson3_task6/Program.cs
@@ -0,0 +1,21 @@
+﻿// 6.  Нахождение числа
+// Создайте программу, которая будет принимать число (n), введенное пользователем, и выдавать результат 
+//в виде (n + n * 2).
+// Пример:
+// // Вводим 23
+// // Получаем: 2346
+
+using System;
+
+namespace lesson3_task6
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{num}{num*2}");
+        }
+    }
+}
