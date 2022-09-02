55cd057060ec6c0d979ece42ba4bff2d7a222f35
LesyaLarina <69485004+LesyaLarina@users.noreply.github.com>
2022-09-02 12:12:45 +0300

    Ternary operator

---
+++ lesson5_task0/Program.cs
@@ -0,0 +1,20 @@
+﻿// Создайте программу для ввода в консоль двух произвольных чисел. При помощи тернарного оператора 
+// сформулируйте ответ программы: либо деление полученных чисел, либо вывод 1.
+// Подсказка: для конвертации в тип данных float используйте ToSingle.
+
+using System;
+
+namespace lesson5_task0
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите 2 числа:");
+            float num1 = Convert.ToSingle(Console.ReadLine ());
+            float num2 = Convert.ToSingle(Console.ReadLine ());
+            float res = num2 !=0 ? num1/num2 : 1;
+            Console.WriteLine(res);
+        }
+    }
+}
