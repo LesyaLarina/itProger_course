808306a812a8fa23f112fac0a0e135d465463363
LesyaLarina <69485004+LesyaLarina@users.noreply.github.com>
2022-09-02 11:23:01 +0300

    Empty string

---
+++ lessons3-5_task8/Program.cs
@@ -0,0 +1,23 @@
+﻿// Пустая строка
+// Создайте программу для получения строки от пользователя.
+// Если пользователь будет вводить пустую строку, то программа будет заново запрашивать строку. Программа 
+// должна запрашивать строку до тех пор, пока пользователь не введет какое-либо значение с клавиатуры.
+
+using System;
+
+namespace lessons3_5_task8
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите текст");
+            string? text = Console.ReadLine();
+            while (string.IsNullOrWhiteSpace(text)) 
+            {Console.WriteLine("Попробуйте еще раз");
+            text = Console.ReadLine();}
+           
+        }   
+         
+    }
+}
