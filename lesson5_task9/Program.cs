12529c21ffa44223bc28596dc7369c578d110d5b
LesyaLarina <69485004+LesyaLarina@users.noreply.github.com>
2022-09-13 17:43:38 +0300

    Even figures

---
+++ lesson5_task9/Program.cs
@@ -0,0 +1,28 @@
+﻿// Четные числа
+// Из двух чисел с разной четностью вывести на экран четное число. a, b - данные числа.
+// Если оба числа четные выведите - "Оба числа четные". Если оба числа не четные выведите - 
+// "Нет четных чисел".
+// Числа необходимо получить от пользователя с клавиатуры.
+
+using System;
+
+namespace lesson5_task9
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите 2 числа");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0 && b % 2 == 0)
+                Console.WriteLine("Оба числа четные");
+            else if (a % 2 != 0 && b % 2 != 0)
+                Console.WriteLine("Нет четных чисел");
+            else if (a % 2 != 0 && b % 2 == 0)
+                Console.WriteLine($"{b} четное");
+            else 
+                Console.WriteLine($"{a} четное");
+        }
+    }
+}
