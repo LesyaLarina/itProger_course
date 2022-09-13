331ee5569cbf49767870d72683d00e4ef94f3e7a
LesyaLarina <69485004+LesyaLarina@users.noreply.github.com>
2022-09-13 16:27:55 +0300

    Conditionals

---
+++ lesson5_task4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Условные операторы
+// Создайте переменную, в которой будет указан возраст пользователя, соблюдая при этом следующие 
+// условия проверки:
+// если возраст больше 18 – на экран выводиться предложение: «You can do everything»;
+// если возраст равен 18 – «Hoho, you're 18!»;
+// если меньше 18 – «You are too young».
+
+using System;
+
+namespace lesson5_task4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите возраст:");
+            byte age = Convert.ToByte(Console.ReadLine());
+            if (age > 18) 
+                Console.WriteLine("You can do everything");
+            else if (age == 18) 
+                Console.WriteLine("Hoho, you're 18!");
+            else Console.WriteLine("You are too young");
+        }
+        
+    }
+}
