c9f707c9a782391b7e8ff9d43c58f0d1640c7345
LesyaLarina <69485004+LesyaLarina@users.noreply.github.com>
2022-09-13 16:18:51 +0300

    Simple accounting

---
+++ lesson5_task3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Обычная бухгалтерия
+// Создайте программу, которая будет рассчитывать сальдо торгового баланса. В случае положительного сальдо 
+// (превышение экспорта над импортом) просчитывать прибыль, а в случае отрицательного сальдо 
+// (превышение импорта над экспортом) рассчитать потери.
+// Например:
+// // Переменные: потери - 750 и прибыль - 345
+// // Программа выдаст результат:
+// "Ваши убытки составили: 405"
+// // В случае прибыли будет подсчитана прибыль
+
+using System;
+
+namespace lesson5_task3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Выручка: ");
+            int profit = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Потери: ");
+            int loss = Convert.ToInt32(Console.ReadLine());
+            if (profit > loss) 
+                Console.WriteLine($"Прибыль: {profit - loss}");
+            else if (profit < loss)
+                Console.WriteLine($"Убыток: {loss - profit}");
+            else Console.WriteLine("Вышли в ноль");
+
+        }
+    }
+}
